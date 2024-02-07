627111e458e398053d1569353d88d4fd01e3b561
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-02-07 17:40:38 +0200

    Lead Magnets Pages

---
+++ code-nexa/app/(dashboard)/lead-magnet/page.tsx
@@ -0,0 +1,68 @@
+import { prismadb } from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import React from "react";
+
+
+const getLeadMagnets = async (userId: string) => {
+  try {
+    const leadMagnets = await prismadb.leadMagnet.findMany({
+      where: { userId },
+    });
+
+    return leadMagnets;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+const getLeads = async (userId: string) => {
+  try {
+    const leads = await prismadb.lead.findMany({
+      where: { userId },
+    });
+
+    return leads;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+const getSubscription = async (userId: string) => {
+  try {
+    const subscription = await prismadb.subscription.findUnique({
+      where: { userId },
+    });
+
+    return subscription;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+async function LeadMagnetsPage() {
+  const { userId } = auth();
+
+  console.log("userId", userId);
+
+  if (!userId) return <div>No user found...</div>;
+
+  const leadMagnetsRequest = getLeadMagnets(userId);
+  const leadsRequest = getLeads(userId);
+  const subscriptionRequest = getSubscription(userId);
+
+  const [leadMagnets, leads, subscription] = await Promise.all([
+    leadMagnetsRequest,
+    leadsRequest,
+    subscriptionRequest,
+  ]);
+
+  console.log("leadMagnets", leadMagnets);
+  console.log("leads", leads);
+
+  return <div>LeadMagnetsPage</div>
+}
+
+export default LeadMagnetsPage;
