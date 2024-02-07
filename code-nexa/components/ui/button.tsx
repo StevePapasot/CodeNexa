63676304dd208b1a58c8b720c2ed2a4edcc5f552
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-02-07 13:56:24 +0200

    Fixing the buttons

--- code-nexa/components/ui/button.tsx
+++ code-nexa/components/ui/button.tsx
@@ -9,11 +9,11 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-purple-500 text-white hover:bg-purple-500/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        "border text-white border-purple-500 bg-purple-500 hover:bg-black hover:text-white hover:border-white",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
