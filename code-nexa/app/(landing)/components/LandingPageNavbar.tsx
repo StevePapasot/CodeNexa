4cda1bb45c46897f2b1f285887d8399b74bd827e
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-02-07 14:11:30 +0200

    Navbar and buttons completed

--- code-nexa/app/(landing)/components/LandingPageNavbar.tsx
+++ code-nexa/app/(landing)/components/LandingPageNavbar.tsx
@@ -10,18 +10,15 @@ async function LandingPageNavbar() {
   console.log("USER", user);
 
   return <nav className="flex w-screen items-center justify-between p-6">
-      {/* <div>
-        <img src="../code-nexa/components/images/3.png" alt="CodeNexa-Logo" className="h-8" />
-      </div> */}
       <div>
         <Link
-          className="text-2xl font-bold text-black-500 no-underline"
+          className="text-3xl font-bold text-white no-underline "
           href="/"
         >
           CodeNexaLabs
         </Link>
       </div>
-      <div className="text-black-500 font-semibold text-lg">
+      <div className="text-black font-semibold text-lg">
         {user ? (
           <div className="flex flex-row gap-x-4 items-center">
             <Link href="/lead-magnets">
--- code-nexa/components/ui/button.tsx
+++ code-nexa/components/ui/button.tsx
@@ -5,25 +5,25 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-xl font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-purple-500 text-white hover:bg-purple-500/90",
+        default: "bg-purple-700 text-white hover:bg-purple-700/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-        "border text-white border-purple-500 bg-purple-500 hover:bg-black hover:text-white hover:border-white",
+        "border text-white border-purple-700 bg-purple-700 hover:bg-black hover:text-white hover:border-white",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
+        default: "h-11 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        icon: "h-11 w-11",
       },
     },
     defaultVariants: {
