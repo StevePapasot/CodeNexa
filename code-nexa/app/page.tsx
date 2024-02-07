b478f65403044a119ac0f93c9abb75608a6c65c6
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-02-07 12:21:15 +0200

    Authentication Completed

--- code-nexa/app/page.tsx
+++ code-nexa/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
  
 export default function Home() {
   return (
     <div>
       <Button>Click me</Button>
+      <UserButton />
     </div>
   )
 }
