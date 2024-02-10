0cc9a84f1a5a3f51d5e97248d501dce91969ffd9
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-02-10 20:36:19 +0200

    Making the Editor Page

--- code-nexa/components/LoadingScreen.tsx
+++ code-nexa/components/LoadingScreen.tsx
@@ -5,10 +5,10 @@ function LoadingScreen() {
   return (
     <div className="z-50 flex h-screen w-screen items-center justify-center bg-white">
       <div className="relative flex flex-col items-center justify-center">
-        <div className="z-10 text-5xl font-bold text-purple-500 mb-5">
+        <div className="z-10 text-5xl font-bold text-purple-600 mb-5">
           LeadConvert
         </div>
-        <RingLoader color="rgba(168, 85, 247, 1)" speedMultiplier={1.5} />
+        <RingLoader color="rgba(124, 58, 237, var(--tw-text-opacity))" speedMultiplier={1.5} />
       </div>
     </div>
   );
