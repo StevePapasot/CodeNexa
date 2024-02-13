8e6a872abb780649f6b7eac3c8f5702d8c0b9d92
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2024-02-13 14:39:35 +0200

    Change the inside

--- code-nexa/next.config.js
+++ code-nexa/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ["utfs.io"],
+    },
+  };
+  
+  module.exports = nextConfig;
